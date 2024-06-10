c0e3ee48160bcc3f2a20839de13d4add8946a411
saveriomercante <saveriomercante@gmail.com>
2024-06-10 20:53:51 +0200

    fix finestra

---
+++ constants.js
@@ -0,0 +1,45 @@
+export const scaleFactor = 1.5;
+
+export const dialogueData = {
+  finestra: `Da qui entra una piacevole luce, questa luce naturale migliora il mio umore.`,
+  exit: `Se desideri uscire dal mio portfolio, ti bastera chiudere questa scheda.`,
+  pianta3 : `Il mio bonsai ficus ginseng e' un simbolo di pazienza e perseveranza, 
+  qualita essenziali per affrontare le sfide della programmazione.`,
+  sofa : `Il mio divano e' il luogo ideale per rilassarmi e meditare. La meditazione mi aiuta 
+  a ridurre lo stress, migliorare la concentrazione e trovare nuove idee.`,
+  pianta2 : `Questa dracaena dorado purifica l'aria e aggiunge un tocco di verde al mio spazio, 
+  creando un ambiente di lavoro piu sano e produttivo.`,
+  tveconsole : `La mia tv. Mi permette di rilassarmi e svagare la mente. Adoro guardare video su youtube sulla programmazione. `,
+  genitaly: `Sviluppatore Java di Generation Italy ,e' stato il corso con cui ho iniziato il mio percorso nella programmazione 
+  e mi ha preparato per il mondo del lavoro, insegnandomi a lavorare in team e a risolvere problemi reali.<a href="https://github.com/SavYanmar94/mygamepresentation/blob/main/Diploma%20Generation_Digital%20(5).pptx.pdf" target="_blank">Eccolo qui!</a>!`,
+  cs50harvard: `CS50 di Harvard: Il corso che mi ha aperto gli occhi sul vasto e affascinante mondo della Computer Science,che ti mette alla prova con challenge,  
+  e ti insegna a ragionare come un problem solver, solidificando le basi per la carriera da sviluppatore.<a href="https://certificates.cs50.io/3e4b0979-432c-48d8-a0ff-5e50cfc898af.pdf?size=letter" target="_blank">Eccolo qui!</a>!`,
+  dukeuniversity: `Java Programming and Software Engineering Fundamentals Specialization della Duke University: 
+  Mi ha insegnato i principi fondamentali della programmazione Java e dell'ingegneria del software, fornendomi gli strumenti per risolvere problemi complessi del mondo reale e costruire applicazioni solide e scalabili. <a href="https://www.coursera.org/account/accomplishments/specialization/9EYPU585KPCU" target="_blank">Eccolo qui!</a>!`,
+  pianta1 : `Il mio lucky bamboo porta fortuna e prosperita, e mi ricorda anche l'importanza 
+  di una crescita costante, sia nella vita che nel lavoro.`,
+  meowth: `Lui e' Meowth, il mio gatto. Un compagno di lavoro indispensabile, sempre pronto a offrirmi supporto morale 
+  (e qualche divertente diversivo) durante le lunghe sessioni di programmazione.`,
+  postazione: `Questa e' la mia postazione PC. Lavoro principalmente in Java, ma mi piacciono 
+  anche Javascript e Python. In generale, amo imparare cose nuove e affrontare nuove sfide.
+  Spesso sviluppo siti web per clienti privati .
+  Dai un'occhiata al mio <a href="https://github.com/SavYanmar94" target="_blank">Github</a>!.`,
+  progetti: `Questa e' la mia postazione PC. Lavoro principalmente in Java, ma mi piacciono 
+  anche Javascript e Python. Spesso realizzo anche siti web per clienti privati. 
+  In generale, amo imparare cose nuove e affrontare nuove sfide. 
+  Dai un'occhiata al mio <a href="https://github.com/SavYanmar94" target="_blank">Github</a>!`,
+  vetrata: `Qui conservo i miei ricordi piu cari e alcuni oggetti da collezione. Mi ricordano l'importanza di 
+  bilanciare il lavoro con le passioni personali.`,
+  scrivaniacv: `Questa e' la mia scrivania, e su di essa c'e' il mio curriculum. <a href="https://github.com/SavYanmar94/mygamepresentation/blob/main/Cv%20Junior%20Java%20Dev%20.pdf" target="_blank">Dagli un'occhiata?</a> 
+  Contattami a saveriomercante@gmail.com se hai opportunita di lavoro interessanti!`,
+  cibo: `Mangiare sano e' fondamentale per mantenere alta l'energia e la concentrazione, soprattutto quando si lavora su progetti impegnativi.`,
+  cucina: `In cucina preparo pasti salutari e sperimento nuove ricette, un ottimo modo per 
+  stimolare la creativita anche fuori dal lavoro.`,
+  letto: `Questa e' la mia oasi di riposo, dove ricarico le energie per affrontare al meglio 
+  ogni giornata. Un sonno ristoratore e' fondamentale per mantenere alta la concentrazione e la creativita, elementi essenziali per un programmatore.`,
+  libreria: `I miei scaffali sono pieni di libri di crescita personale . 
+  C'e' sempre qualcosa di nuovo da imparare e la conoscenza e' un viaggio senza fine.`,
+  orologio: `Questo orologio non solo mi aiuta a tenere traccia del tempo con stile, ma mi ricorda anche l'importanza di ogni minuto. 
+  Il tempo e' prezioso e va investito in cio che amiamo.`,
+ 
+};
